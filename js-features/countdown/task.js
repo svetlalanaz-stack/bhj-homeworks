2866bf368b569fc7d1eaa431022bdd9b4376dc12
Svetlana Zlt <svetlalanaz@gmail.com>
2025-12-10 00:32:04 +0300

    initial Commit

---
+++ js-features/countdown/task.js
@@ -0,0 +1,10 @@
+const timerValue = function() {
+    let timer = document.getElementById("timer");
+    if(timer.textContent == 0) {
+        alert("Вы победили в конкурсе");
+        clearInterval(timerInterval);
+    } else {
+        timer.textContent--;
+    }
+}
+const timerInterval = setInterval(timerValue, 1000);
